bdc3a99f267f492f252575dd6a058eef4e3c3238
breadream <brad.dh.lim@gmail.com>
2018-10-01 23:44:04 -0500

    Fixed the issue

--- instagram/config/routes.rb
+++ instagram/config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users,
   	path: '',
 	path_names: {sign_in: 'login', sign_out: 'log_out', edit: 'profile', sign_up:'registration'},
-	contorllers: {omniauth_callbacks: 'omniauth_callbacks'}
+	controllers: {omniauth_callbacks: 'omniauth_callbacks'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
